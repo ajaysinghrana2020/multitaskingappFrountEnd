01ea3f723e729b778c59c44123d5cf1f8a61f0bf
ajay <ajaysinghrana8894@gmail.com>
2024-01-31 20:00:28 +0530

    changed my mind now making a KBC application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NotesComponent } from './notes/notes.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
-  {path:'',component:LoginComponent},
-  {path:'notes',component:NotesComponent}
+  // {path:'',component:LoginComponent},
+  {path:'notes/:id',component:NotesComponent},
+  {path:'',component:LandingPageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,24 @@ import { LoginComponent } from './login/login.component';
 import { NotesComponent } from './notes/notes.component';
 import {HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { QuestionsPageComponent } from './landing-page/questions-page/questions-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NotesComponent
+    NotesComponent,
+    LandingPageComponent,
+    QuestionsPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
